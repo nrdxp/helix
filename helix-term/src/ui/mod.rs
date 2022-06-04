1dc2381ac6340642680d6063c362023ff212c1fe
Philipp Mildenberger <philipp@mildenberger.me>
2022-06-04 13:10:47 +0200

    Fixed various things with path completion

--- helix-term/src/ui/mod.rs
+++ helix-term/src/ui/mod.rs
@@ -12,7 +12,7 @@ mod spinner;
 mod statusline;
 mod text;
 
-pub use completion::{Completion, CompletionItem, PathType};
+pub use completion::{Completion, CompletionItem, PathType, PATH_REGEX};
 pub use editor::EditorView;
 pub use markdown::Markdown;
 pub use menu::Menu;
